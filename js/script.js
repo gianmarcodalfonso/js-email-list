73495d404140b4962fdcb69621646cfab9a1d7c5
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-18 16:05:12 +0200

    fix: now rndm10Mail generates 10 different emails

--- js/script.js
+++ js/script.js
@@ -2,15 +2,14 @@ const rndmMailBool = `https://flynn.boolean.careers/exercises/api/random/mail`
 const mailList = document.getElementById(`mail-list`)
 
 const rndm10mail = ((list) => {
-  axios.get(rndmMailBool).then((res) => {
-
     list.innerHTML = ``
     for(let i = 0; i < 10; i++) {
+      axios.get(rndmMailBool).then((res) => {
       let rndmMail = res.data.response
       list.innerHTML += `<li>${rndmMail}</li>`
+    })
     }
     console.log(mailList)
-  })
 })
 
 rndm10mail(mailList)
