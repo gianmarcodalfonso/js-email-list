36b58265ecf050f2dd5d112d62bc4fc7f9c3063b
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-18 15:59:21 +0200

    feat: funtion to generate 10 rndmMail

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 const rndmMailBool = `https://flynn.boolean.careers/exercises/api/random/mail`
 const mailList = document.getElementById(`mail-list`)
 
+const rndm10mail = ((list) => {
+  axios.get(rndmMailBool).then((res) => {
+
+    list.innerHTML = ``
+    for(let i = 0; i < 10; i++) {
+      let rndmMail = res.data.response
+      list.innerHTML += `<li>${rndmMail}</li>`
+    }
+    console.log(mailList)
+  })
+})
+
+rndm10mail(mailList)
